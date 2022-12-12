8ac8047d8fba05fbcd727bd73a2aa64e5249da95
terryhycheng <terryhycheng@gmail.com>
2022-12-12 15:04:44 +0000

    Refactored directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -5,9 +6,14 @@ puts "Darth Vader"
 puts "Nurse Ratched"
 puts "Michael Corleone"
 puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
+puts ;"The Wicked Witch of the West"
 puts "Terminator"
 puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# finally, we print the total number of students
+print "Overall, we have "
+#it's important that print() doesn't add new line characters
+print 11
+puts " great students"
