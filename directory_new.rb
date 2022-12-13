b7a6635ca60b329b99e62a4b3212785cf41678e5
terryhycheng <terryhycheng@gmail.com>
2022-12-13 11:36:17 +0000

    Added hobbies & country of birth inputs, added String.center

--- directory_new.rb
+++ directory_new.rb
@@ -9,7 +9,7 @@ def input_students
     # while the name is empty, repeat this code
     while !name.empty? do
         # add the student hash to the array
-        students << { name: name, cohort: :november }
+        students << { name: name, cohort: :November, country_of_birth: input_country_of_birth(name) , hobbies: input_hobbies(name) }
         puts "Now we have #{students.count} students"
         # get another name from the user
         name = gets.chomp
@@ -18,40 +18,70 @@ def input_students
     students
 end
 
+def input_country_of_birth(name)
+    # get country of birth
+    puts "Please enter where #{name} was born"
+    country_of_birth = gets.chomp
+    # if user enters nothing, repeat the code
+    while country_of_birth.empty?
+        country_of_birth = gets.chomp
+    end
+    # return country of birth
+    country_of_birth
+end
+
+def input_hobbies(name)
+    # create an empty array
+    hobbies = []
+    # get hobby
+    puts "Please enter #{name}'s hobbies"
+    puts "Hit enter twice to go to next step"
+    hobby = gets.chomp
+
+    # while the hobby is empty, repeat this code
+    while !hobby.empty?
+        hobbies << hobby
+        puts "Now #{name} has #{hobbies.count} hobbies"
+        # ask another hobby
+        hobby = gets.chomp
+    end
+
+    # return a list of hobbies
+    hobbies
+end
+
 def input_special_letter
+    # get a special letter for filter
     puts "Please indicate a special letter for filtering. (Optional)"
     special_letter = gets.chomp
 end
 
 def print_header(special_letter)
-    puts "The students of Villains Academy"
+    puts "The students of Villains Academy".center(100)
     if !special_letter.empty?
-        puts "With '#{special_letter.upcase}' letter filter"
+        puts "With '#{special_letter.upcase}' letter filter".center(100)
     end
-    puts "-------------"
+    puts "-------------".center(100)
 end
 
 def print_students(students, special_letter)
     index = 0
-    
+    # check if there is any special letter filter
     if !special_letter.empty?
+        # select only student whose name begins with special letter
         students = students.select do |student|
             student[:name][0].downcase == special_letter.downcase
         end
     end
-    
+    # loop through the students array
     while index < students.length
-        if students[index][:name].length < 12
-            puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
+        student = students[index]
+        if student[:name].length < 12
+            # then print them out
+            puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort, hobbies: #{student[:hobbies]}, born in: #{student[:country_of_birth]})".center(100)
         end
         index += 1
     end
-
-    # students.each_with_index do |student, index|
-    #     if student[:name].length < 12
-    #         puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    #     end
-    # end
 end
 
 def print_footer(students)
