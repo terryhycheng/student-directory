1d45567d983a476b4cf1f0b4eb7e27a2d803efe4
terryhycheng <terryhycheng@gmail.com>
2022-12-14 13:10:39 +0000

    Added interactive menu and refactored

--- directory.rb
+++ directory.rb
@@ -1,21 +1,51 @@
+def interactive_menu
+    @students = []
+    loop do
+        print_menu
+        process(gets.chomp)
+    end
+end
+
+def process(selection)
+    case selection
+        when "1"
+            input_students
+        when "2"
+            show_students
+        when "9"
+            exit # this will cause the program to terminate
+        else
+            puts "I don't know what you meant, try again"
+        end
+end
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students
+  print_footer
+end
+
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit enter twice"
 
-    # create an empty array
-    students = []
     # get the first name
     name = gets.chomp
     # while the name is empty, repeat this code
     while !name.empty? do
         # add the student hash to the array
-        students << { name: name, cohort: :november }
-        puts "Now we have #{students.count} students"
+        @students << { name: name, cohort: :november }
+        puts "Now we have #{@students.count} students"
         # get another name from the user
         name = gets.chomp
     end
-    # return the array of students
-    students
+
 end
 
 def print_header
@@ -23,17 +53,14 @@ def print_header
     puts "-------------"
 end
 
-def print_students(students)
-    students.each do |student|
+def print_students
+    @students.each do |student|
         puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
-def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
+def print_footer
+    puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header()
-print_students(students)
-print_footer(students)
+interactive_menu
